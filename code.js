8a6ed76aa4e9855107c6d0f0dcf61959e00b81f9
JJ <jc@orbsdigital.com>
2024-01-24 18:33:11 +1100

    added function to create random output

---
+++ code.js
@@ -0,0 +1,20 @@
+//selects aand extract all DOM element that we will work with
+const squares = document.querySelector('.square')
+const mole = document.querySelector('.mole')
+const time = document.querySelector('#time-remaining')
+const score = document.querySelector('#score')
+
+let result = 0;
+
+//function to randomise square output
+function randomSquare(){
+    squares.forEach(square => {
+        squares.classList.remove('mole')//if mole exist in a square remove it
+    })
+
+    let randomPosition = square[Math.floor(Math.random()*9)]// pass random square into an array 1-9
+    console.log(randomPosition)
+    console.log(Math.floor(Math.random()*9))
+
+}
+randomSquare ()
